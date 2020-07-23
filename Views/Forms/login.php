77cc3fab21b07c425703f83405b9ea15217780fb
Bernd Engels <engels@goldenacker.de>
2020-07-23 13:30:49 +0200

    login form

--- Views/Forms/login.php
+++ Views/Forms/login.php
@@ -11,13 +11,13 @@
     <div class="form-group row">
         <label for="password" class="col-md-2 col-form-label">Passwort</label>
         <div class="col-md-10">
-            <input type="password" id="password" name="password" class="form-control col-sm-12 col-md-6 px-1" />
+            <input type="password" id="password" name="password" class="form-control col-sm-12 col-md-6 px-1" required />
         </div>
     </div>
 
     <div class="form-group row">
         <div class="col-md-auto float-right">
-            <input type="submit" id="login" name="login" value="Login" role="button" class="btn btn-primary col-md-auto px-5" />
+            <input type="submit" id="login" name="login" value="Login" role="button" class="btn btn-primary col-md-auto px-5" required />
         </div>
     </div>
 </form>
