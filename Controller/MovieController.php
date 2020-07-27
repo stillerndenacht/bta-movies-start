df59952a8f93d670caa944f7f57ac9ce0a1d39b3
stillerndenacht <61967922+stillerndenacht@users.noreply.github.com>
2020-07-27 12:04:18 +0200

    Update MovieController.php

--- Controller/MovieController.php
+++ Controller/MovieController.php
@@ -1,24 +1,29 @@
 <?php
 
 require_once 'Models/Movie.php';
+require_once 'Controller.php';
 
-class MovieController {
+class MovieController extends Controller{
 
     /**
      * @var Movie
      */
-    private $model;
+    /*das Model wird vom übergeordneten Controller.php geerbt
+    protected $model;*/
+    protected $listTitle = 'Movie';
+    protected $listView = 'Views/movies.php';
 
     public function __construct() {
         $this->model = new Movie;
     }
 
-    public function index()
+   /* diese Funktion wird jetzt vom übergeordneten Controller.php geerbt
+   public function index()
     {
         $title   = 'Movie';
         $list    = $this->model->all();
         require_once 'Views/movies.php';
-    }
+    }*/
 
     public function show($id)
     {
