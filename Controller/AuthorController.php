136a12d2a222b8e38eff311097e9bce803dc5caa
Bernd Engels <engels@goldenacker.de>
2020-07-27 13:20:52 +0200

    author store

--- Controller/AuthorController.php
+++ Controller/AuthorController.php
@@ -23,18 +23,42 @@ class AuthorController extends Controller {
     public function edit($id = null)
     {
         $title = 'Edit Autor';
-        if( $id ) {
+        $data  = null;
+        if( $id > 0 ) {
             // existierender author
             $data = $this->model->find($id);
-        } else {
-            // neuer author
-        }
+        } 
         require_once 'Views/Forms/author.php';
     }
 
     public function store($id = null)
     {
-        die(__METHOD__ . ' ID: ' . ($id ?: 'null') );
+        // wir speichern unsere formular daten in variablen
+        $firstname  = $_POST['firstname'];
+        $lastname   = $_POST['lastname'];
+        // todo: server-seitige validierung der form daten 
+        if($id > 0 ) {
+            // author existiert bereits
+            $sql = "UPDATE authors SET firstname = :firstname, lastname = :lastname WHERE id = :id";
+            $params = [
+                'firstname' => $firstname,
+                'lastname'  => $lastname,
+                'id'        => $id, 
+            ];
+
+        } else {
+            // author muss neu angelegt werden
+            $sql = "INSERT IGNORE INTO authors (firstname, lastname) VALUES (:firstname , :lastname)";
+            $params = [
+                'firstname' => $firstname,
+                'lastname'  => $lastname
+            ];
+        }
+
+        $stmt = $this->model->prepare($sql);
+        $stmt->execute($params);
+        // redirect zur listen ansicht
+        header('location: /authors');
     }
 
     public function delete($id)
