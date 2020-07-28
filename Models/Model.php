8e4d799dd357eb02b7e1805b7c4630cef4acc248
Bernd Engels <engels@goldenacker.de>
2020-07-28 15:33:45 +0200

    update

--- Models/Model.php
+++ Models/Model.php
@@ -18,5 +18,36 @@ class Model extends MyDB {
 
         return $result;
     }
+
+    public function update($params, $id)
+    {
+        // baue mir ein array mit spaltenname und spalten platzhalter anhand der $params
+        $updateValues = [];
+        // array_keys liefert alle keys eines arrays als array 
+        foreach (array_keys($params) as $key) {
+            // z.B: 'title = :title'
+            $updateValues[] = "$key = :$key";
+        }
+        // baue mir eine zeichenkette aus dem $updateValues array mit komma als verbindungs-zeichen
+        // z.B: 'title = :title, price = :price, auhtor_id = :author_id ....'  
+        $strUpdateValues = implode(', ', $updateValues);
+        // setze meine SQL anweisung zusammen 
+        $sql = "UPDATE movies SET $strUpdateValues WHERE id = :id";
+        // preapare statment anhand SQL anweisung
+        $stmt = $this->prepare($sql);
+        // füge die id als assoziatives array den $params hinzu
+        $params += ['id' => $id];
+        // führe über execute das SQL kommando aus anhand der parameter in $params
+        $result = $stmt->execute($params);
+        // gebe fehlermeldungen aus, wenn was schiefgelaufen ist 
+        $this->_handleErrors($stmt);
+        
+        return $result;
+    }
+
+    public function insert($params) 
+    {
+
+    }
 }
 ?>
